dc68f3a89520463d00122109fef88356a687b970
CatAway <cataway11@163.com>
2015-12-19 21:10:00 +0800

    Update myEvent.js

--- myEvent.js
+++ myEvent.js
@@ -1,9 +1,8 @@
 /**
- * Created by cataway on 2015/9/9.
+ * Created by cataway
  */
 
-function on(ele,type,fn){
-    //�Զ����¼�
+function on(ele,type,fn){ //负责“约定”，就把约定某事件的这些方法有序保存在数组里
     if(/^self/.test(type)){
         if(!ele["selfEvent"+type]){
             ele["selfEvent"+type]=[];
@@ -15,9 +14,9 @@ function on(ele,type,fn){
         a.push(fn);
         return;
     }
-    //ϵͳ�¼�
     if(!ele["aEvent"+type]){
         ele["aEvent"+type]=[];
+        //表示无论什么浏览器，都使用run方法来处理事件绑定
         if(ele.addEventListener){
             ele.addEventListener(type,run,false);
         }else{
@@ -25,12 +24,13 @@ function on(ele,type,fn){
         }
     }
     var a=ele["aEvent"+type];
+    //避免重复保存的一个循环判断
     for(var i=0;i< a.length;i++){
         if(a[i]==fn)return;
     }
     a.push(fn);
 }
-function run(e){
+function run(e){ //当事件触发的时候，真正执行的是run方法，它负责按顺序去执行原先保存在数组里的那些方法（由on保存的）
     e=e||window.event;
     var a=this["aEvent"+ e.type];
     if(a){
@@ -49,7 +49,7 @@ function run(e){
     }
 }
 
-function selfRun(selfType,e){//selfType���Զ����¼����ͱ�ʶ��
+function selfRun(selfType,e){ //selfType是自定义的事件类型的标识符，e是系统事件对象
     var a=this["selfEvent"+selfType];
     if(a){
         for(var i=0;i< a.length;i++){
